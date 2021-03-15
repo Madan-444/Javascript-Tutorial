00c1bd2f218d624312d68e3f98bcb854f36f7373
Madan-444 <mmadan3600@gmail.com>
2021-03-15 22:35:26 +0545

    2021 started

---
+++ 2021 Js/021js.js
@@ -0,0 +1,11 @@
+console.log('Hello madan')
+
+const myName = "Madan Rijal Magar"
+console.log(typeof myName)
+function namingPro(){ 
+    let myName = "Becool Rijal"
+    console.log("My name is: ",myName)
+}
+namingPro()
+
+console.log('fucking name',myName)
